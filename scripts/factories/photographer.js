6678b9d5d05cb66e6101dcd6f8387b2791e98b72
Gamze Erdogan <ckrgamze@gmail.com>
2022-03-28 16:10:08 +0200

    finished the page index

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
-
+    const pageAdress = 'photographer.html';
+        
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const section = document.createElement('section')
+        const link_a = document.createElement( 'a' );
+        const infoAside = document.createElement('aside');
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const titleH4 = document.createElement( 'h4');
+        const tagLine = document.createElement ('p');
+        const money = document.createElement('p');
+        infoAside.classList.add('photographerAside');
+        img.setAttribute("src", picture);
+        link_a.setAttribute("href",pageAdress);
+        infoAside.setAttribute("aria-label", 'information');
+        titleH4.classList.add("photographerTitle");
+        tagLine.classList.add("photographerTag");
+        money.classList.add("photographerMoney");
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        titleH4.textContent = city + ', ' + country;
+        tagLine.textContent = tagline;
+        money.textContent = price +'€/jour';
+        section.appendChild(link_a);
+        section.appendChild(infoAside)
+        link_a.appendChild(img);
+        link_a.appendChild(h2);
+        infoAside.appendChild(titleH4);
+        infoAside.appendChild(tagLine);
+        infoAside.appendChild(money);
+        
+        return (section);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,15 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+            let url = '/data/photographers.json';
+            try {
+                let res = await fetch (url);
+                return await res.json();
+                
+            } catch (error){
+                console.log(error)
+            }
+            
+        }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -37,8 +23,11 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        const photographersObj = await getPhotographers();
+        console.log("result: ",photographersObj);
+        displayData(photographersObj.photographers);
+        //photographersObj.media
+  
     };
     
     init();
